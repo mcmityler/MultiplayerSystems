bbe41403d963bbbff7ca36de9f1b7d3629053f91
mcmityler <mcmityler@gmail.com>
2020-03-01 17:20:17 -0500

    TicTacToe Base
    Base for tic tac toe game

---
+++ TicTacToe/Assets/Scripts/Space.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Space : MonoBehaviour
+{
+    public Button button;
+    public Text buttonText;
+    private GameController gameController;
+
+    public void SetControllerReference(GameController control)
+    {
+        gameController = control;
+    }
+
+    public void SetSpace()
+    {
+        buttonText.text = gameController.GetSide();
+        button.interactable = false;
+        gameController.EndTurn();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonText = this.GetComponentInChildren<Text>();
+    }
+    
+}
